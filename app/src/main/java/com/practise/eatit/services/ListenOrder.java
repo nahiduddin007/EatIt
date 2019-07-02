5b32e2a713d5569faa96f1bf7161855137aa407b
Rafat <razhossain201@gmail.com>
2019-07-02 19:45:26 +0600

    There are 1 more issue   -> SearchBar in FoodList

---
+++ app/src/main/java/com/practise/eatit/services/ListenOrder.java
@@ -0,0 +1,106 @@
+package com.practise.eatit.services;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.practise.eatit.R;
+import com.practise.eatit.model.Request;
+import com.practise.eatit.view.OrderStatusActivity;
+
+public class ListenOrder extends Service implements ChildEventListener {
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseReference;
+
+    public ListenOrder() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mDatabase = FirebaseDatabase.getInstance();
+        mDatabaseReference = mDatabase.getReference("Requests");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mDatabaseReference.addChildEventListener(this);
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+    }
+
+    @Override
+    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+        Request request = dataSnapshot.getValue(Request.class);
+        showNotification(dataSnapshot.getKey(), request);
+    }
+
+    private void showNotification(String key, Request request) {
+        Intent intent = new Intent(getBaseContext(), OrderStatusActivity.class);
+        intent.putExtra("userPhone", request.getPhone());
+        PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getBaseContext());
+
+        builder.setAutoCancel(true)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setWhen(System.currentTimeMillis())
+                .setTicker("Abeer Food")
+                .setContentInfo("Your Order was updated!")
+                .setContentText("Order #"+key+" was update status to "+convertCodeToStatus(request.getStatus()))
+                .setContentIntent(contentIntent)
+                .setContentInfo("Info")
+                .setSmallIcon(R.mipmap.ic_launcher);
+        NotificationManager notificationManager = (NotificationManager) getBaseContext().getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(1, builder.build());
+    }
+
+    @Override
+    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+    }
+
+    @Override
+    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+    }
+
+    @Override
+    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+    }
+
+    private String convertCodeToStatus(String status) {
+        if (status.equals("0")){
+            return "Placed";
+        } else if (status.equals("1")){
+            return "On my Way";
+        } else {
+            return "Shipped";
+        }
+    }
+
+}
